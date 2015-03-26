8d8941d137fe42ac61e8472bb91f138274a5e56a
Sebastian Flückiger <sebastian@public-docking-rz-2205.ethz.ch>
2015-03-26 11:06:26 +0100

    token

--- js/blog.js
+++ js/blog.js
@@ -3,7 +3,7 @@ $(function() {
     Parse.$ = jQuery;
  
     // Replace this line with the one on your Quickstart Guide Page
-    Parse.initialize("W8vTW6MTre3g0ScTeyPzqc6Uzj2KZoQ6GBv0j6ZC", "VVayP3EdZ6QH0QMttzpWgeJ2if4f2m8QjA10SaFQ");
+	Parse.initialize("FWBUIa3MVRxeEgnfRyYFZdKHJpRsjX6itOYt7WwJ", "wyB3Wln1vAsiab19q7HrGUboPfvSlplU22LNZlRK");
  
     var TestObject = Parse.Object.extend("TestObject");
     var testObject = new TestObject();
