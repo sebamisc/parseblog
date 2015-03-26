9dd5e0d5040aa8056fe6037344b7fa089bdb5d86
Sebastian Flückiger <sebastian@public-docking-rz-2205.ethz.ch>
2015-03-26 11:10:06 +0100

    blogs

--- js/blog.js
+++ js/blog.js
@@ -5,10 +5,21 @@ $(function() {
     // Replace this line with the one on your Quickstart Guide Page
 	Parse.initialize("FWBUIa3MVRxeEgnfRyYFZdKHJpRsjX6itOYt7WwJ", "wyB3Wln1vAsiab19q7HrGUboPfvSlplU22LNZlRK");
  
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
-    });
- 
+
+    var Blog = Parse.Object.extend("Blog");
+	var Blogs = Parse.Collection.extend({
+	    model: Blog
+	});
+
+	var blogs = new Blogs();
+
+	blogs.fetch({
+	    success: function(blogs) {
+	        console.log(blogs);
+	    },
+	    error: function(blogs, error) {
+	        console.log(error);
+	    }
+	});
+
 });
